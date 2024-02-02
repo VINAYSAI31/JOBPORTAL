9b3ce5b885dea017ba727c5abf02bbb2850b1d05
VINAYSAI31 <vinaysai1133@gmail.com>
2024-02-02 14:51:51 +0530

    LOGIN PAGE

--- jobportal/adminmodule/urls.py
+++ jobportal/adminmodule/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     path('employeerhomepage',views.employeerhomepage,name='employeerhomepage'),
     path('jobseekerhomepage',views.jobseekerhomepage,name='jobseekerhomepage'),
     path('signup',views.signup,name='signup'),
-    path('signup1',views.signup1,name='signup1')
+    path('signup1',views.signup1,name='signup1'),
+    path('login',views.login,name='login'),
+    path('login1',views.login1,name='login1'),
+    path('logout',views.logout,name='logout'),
+
 ]
--- jobportal/adminmodule/views.py
+++ jobportal/adminmodule/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from django.contrib import messages
+from django.shortcuts import render,redirect
+from django.contrib import messages, auth
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 def projecthomepage(request):
     return render(request, 'projecthomepage.html')
@@ -26,12 +27,41 @@ def signup1(request):
                 return render(request, 'signup.html')
             else:
                 user = User.objects.create_user(username=username, password=pass1)
-                user.save()
                 messages.info(request, 'Account created successfully!! ')
+                user.save()
+
                 return render(request, 'projecthomepage.html')
         else:
             messages.info(request, 'Password does not match')
             return render(request, 'signup.html')
 
+def login(request):
+    return render(request, 'login.html')
 
+def login1(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        pass1 = request.POST['password']
+        user = auth.authenticate(username=username, password=pass1)
+
+        if user is not None:
+            auth.login(request, user)
+            if len(username) == 10:
+                return redirect('jobseekerhomepage')
+                messages.success(request, 'Logged in successfully as job seeker!')
+            elif len(username) == 4:
+                return redirect('employeerhomepage')
+                messages.success(request, 'Logged in successfully as employer!')
+                messages.success(request, 'Logged in successfully as employer!')
+            else:
+                messages.success(request, 'Logged in successfully!')
+                return redirect('projecthomepage')
+        else:
+            messages.error(request, 'Invalid Credentials')
+            return render(request, 'login.html')
+    else:
+        return render(request, 'login.html')
+def logout(request):
+    auth.logout(request)
+    return render(request,'projecthomepage.html')
 
